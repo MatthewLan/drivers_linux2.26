bc5590f3c74b60b464332cec41c138d6a979039a
Matthew <tianlang2060@163.com>
2017-08-05 17:33:41 +0800

    add button_timer

---
+++ button_timer/button_drv_test_timer.c
@@ -0,0 +1,26 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <signal.h>
+#include <stdio.h>
+
+
+int main(int argc, char **argv)
+{
+	int fd;
+	unsigned char key_val = 0;
+
+	fd = open("/dev/button", O_RDWR);
+	if (fd < 0) {
+		printf("can't open!!!\n");
+		return -1;
+	}
+
+	while (1) {
+		read(fd, &key_val, 1);
+		printf("[button_signal] key_val: %x\n", key_val);
+	}
+
+	return 0;
+}
